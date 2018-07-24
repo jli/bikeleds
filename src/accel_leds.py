cde2cd05ecf3a7333b4d3d0a7ecca9f9ee06eb0a
dex <dex@circularly.org>
2018-07-24 02:22:00 -0400

    Accel LEDs: removed unneeded state, add button to switch palettes, vary color step speed based on accel

--- src/accel_leds.py
+++ src/accel_leds.py
@@ -1,20 +1,34 @@
 import analogio
 import board
+import digitalio
 import simpleio
-# import pulseio
 import time
 
 import neopixel
 
+NLEDS = 40
+MIN_BRIGHT = 0.1
+MAX_BRIGHT = 0.3
+COLOR_STEP_MULT = 1.0
 
-NLEDS = 90
-COLOR_STEP = 10
+class Button(object):
+    def __init__(self, pin):
+        self._but = digitalio.DigitalInOut(pin)
+        self._but.direction = digitalio.Direction.INPUT
+        self._but.pull = digitalio.Pull.UP
+        self._prev_is_pressed = self._is_pressed()
 
-RAW_COLOR_ARRAY = [
-  (255, 0, 0),
-  (0, 255, 0),
-  (0, 0, 255)
-]
+    def _is_pressed(self):
+        # TODO: why does this need inverting?
+        return not self._but.value
+
+    # This only returns true if button is pressed and wasn't pressed the last
+    # time get_press was called.
+    def get_press(self):
+        prev_pressed = self._prev_is_pressed
+        cur_pressed = self._is_pressed()
+        self._prev_is_pressed = cur_pressed
+        return cur_pressed and not prev_pressed
 
 def intavg(x, y):
     return int((x + y) / 2)
@@ -40,30 +54,35 @@ def call_n_times(f, x, n):
         return x
     return call_n_times(f, f(x), n-1)
 
+def smoothify(n, xs):
+    return call_n_times(smooth, xs, n)
+
 def accel_value(axis):
-    # Convert axis value to float within 0...1 range.
     val = axis.value / 65535
-    # Shift values to true center (0.5).
-    val -= 0.5
-    # Convert to gravities.
-    return val * 3.0
+    val -= 0.5  # Shift values to true center (0.5).
+    return val * 3.0  # Convert to gravities.
 
-COLOR_ARRAY = call_n_times(smooth, RAW_COLOR_ARRAY, 7)
-NCOLORS = len(COLOR_ARRAY)
+RGB = smoothify(7, [(255, 0, 0),
+                    (0, 255, 0),
+                    (0, 0, 255)])
+GREEN_BLUE = smoothify(5, [(0, 130, 60),
+                           (0, 50, 100)])
+VIOLET = smoothify(4, [(100, 0, 125),
+                       (50, 0, 90),
+                       (90, 0, 50)])
+ORANGE_PURPLE = smoothify(5, [(150, 50, 0),
+                              (125, 0, 125)])
+PALETTES = [RGB, GREEN_BLUE, VIOLET, ORANGE_PURPLE]
+PALETTE_INDEX = 0
 
-ZERO_POINT = (0,0,0)
-N_ACCEL_POINTS = 10
-ACCEL_POINTS = [ZERO_POINT] * N_ACCEL_POINTS
-ACCEL_POINTS_INDEX = 0
-
-AVG_MOVEMENT = 0
+LAST_ACCEL_VAL = (0, 0, 0)
+AVG_ACCEL_CHG = 0
 EWMA_WEIGHT = 0.05  # for new points
 
-
+palette_but = Button(board.D11)
 
 ## setup LEDs and smoothed color array
 
-# led = pulseio.PWMOut(board.D13, frequency=5000, duty_cycle=0)
 board_led = simpleio.DigitalOut(board.D13)
 
 # on-board metro neopixel
@@ -75,14 +94,14 @@ except AttributeError:
 if metro_neo:
     metro_neo.brightness = 0.01
 
-neos = neopixel.NeoPixel(board.D2, NLEDS)
+neos = neopixel.NeoPixel(board.D12, NLEDS)
 neos.brightness = .1
 
 ## setup accelerometer
 
-x_axis = analogio.AnalogIn(board.A2)
+x_axis = analogio.AnalogIn(board.A0)
 y_axis = analogio.AnalogIn(board.A1)
-z_axis = analogio.AnalogIn(board.A0)
+z_axis = analogio.AnalogIn(board.A2)
 
 def get_new_accel_point():
     return accel_value(x_axis), accel_value(y_axis), accel_value(z_axis)
@@ -91,49 +110,40 @@ def squared_dist(p1, p2):
     (x1, y1, z1), (x2, y2, z2) = p1, p2
     return (x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2
 
-def add_accel_point():
-    global ACCEL_POINTS_INDEX, AVG_MOVEMENT
-    prev = ACCEL_POINTS[ACCEL_POINTS_INDEX]
-    ACCEL_POINTS_INDEX = (ACCEL_POINTS_INDEX + 1) % N_ACCEL_POINTS
+def get_accel_change():
+    global LAST_ACCEL_VAL, AVG_ACCEL_CHG
+    last = LAST_ACCEL_VAL
     cur = get_new_accel_point()
-    ACCEL_POINTS[ACCEL_POINTS_INDEX] = cur
-    move = min(squared_dist(prev, cur) * 30, 5)
-    AVG_MOVEMENT = EWMA_WEIGHT * move + (1-EWMA_WEIGHT) * AVG_MOVEMENT
-
-def last_movement():
-    prev = ACCEL_POINTS_INDEX - 1
-    if prev < 0:
-        prev = -1
-    return squared_dist(ACCEL_POINTS[prev],
-                        ACCEL_POINTS[ACCEL_POINTS_INDEX])
-
-def movements():
-    reordered = ACCEL_POINTS[ACCEL_POINTS_INDEX:] + ACCEL_POINTS[:ACCEL_POINTS_INDEX]
-    return [squared_dist(p1, p2) for (p1, p2) in zip(reordered, reordered[1:])]
+    # Increase by 30x, limit to 5.0
+    raw = squared_dist(last, cur)
+    move = min(raw * 30, 5)
+    AVG_ACCEL_CHG = EWMA_WEIGHT * move + (1-EWMA_WEIGHT) * AVG_ACCEL_CHG
+    LAST_ACCEL_VAL = cur
+    return raw, move, AVG_ACCEL_CHG
 
 
 board_led.value = True
-print('ncolors:', NCOLORS)
-
 
 i = 0
 while True:
-    i = (i+COLOR_STEP) % NCOLORS
-    #print('shift', i, NLEDS, NCOLORS)
-    # add_accel_point()
-    # scaled_move = min(1, max(0.05, AVG_MOVEMENT))
-    #print(ACCEL_POINTS[ACCEL_POINTS_INDEX])
-    #print ((AVG_MOVEMENT, scaled_move,))
-    # neos.brightness = scaled_move
+    if palette_but.get_press():
+        PALETTE_INDEX = (PALETTE_INDEX + 1) % len(PALETTES)
+    palette = PALETTES[PALETTE_INDEX]
 
+    raw, cooked, baked = get_accel_change()
+    scaled = min(MAX_BRIGHT, max(MIN_BRIGHT, baked))
+    print('({:.3f}, {:.3f}, {:.3f}, {:.3f})'.format(raw, cooked, baked, scaled))
+
+    neos.brightness = scaled
+
+    # TODO: hm, does it make sense?
+    step = max(2, int(len(palette) / NLEDS * 0.1 * COLOR_STEP_MULT))
+    # TODO: tweak.
+    step *= int(baked * 20)
+
+    i = (i + step) % len(palette)
     for j in range(NLEDS):
-        index = i + j
-        if index >= NCOLORS:
-            index = 0
-        (r, g, b) = COLOR_ARRAY[index]
-        neos[j] = (r,g,b)
+        neos[j] = palette[(i + j) % len(palette)]
     if metro_neo:
         metro_neo[0] = COLOR_ARRAY[i]
     board_led.value = not board_led.value
-    #time.sleep(.1)
-
