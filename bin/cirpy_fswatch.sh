d2dc68e8b28d63aa1632dfc6d5a6d1385ae03d3c
dex <dex@circularly.org>
2018-07-25 07:40:39 -0400

    add fswatch script

---
+++ bin/cirpy_fswatch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# fswatch is an OS X program like inotifywatch. `brew install fswatch`
+
+DEST=/Volumes/CIRCUITPY/code.py
+
+echo initial copy ...
+/bin/cp -vf "$1" "$DEST"
+echo
+echo watching: "$1"  ...
+fswatch "$1" | while read i; do echo  $(date '+:%H:%M:%S'); /bin/cp -vf "$i" "$DEST"; done
